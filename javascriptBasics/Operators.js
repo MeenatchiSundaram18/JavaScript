15e9e0c752892b2ded5d69e43523a6e9e82f817c
meenatchisundaram <meenatchisundaram730@gmail.com>
2025-09-18 12:40:38 +0530

    operators added

---
+++ javascriptBasics/Operators.js
@@ -0,0 +1,31 @@
+//Comparison Operator
+
+console.log(50 > 30)//true
+console.log(30 >= 30)//true
+console.log(30 >= 50)//false
+console.log(40 == 40)//true
+console.log(100 == "100")//true
+console.log(100 === "100")//false
+
+//Comparison With Strings
+console.log("<-----Comparison With Strings------>")
+
+console.log("apple" >= "banana")//false
+console.log("glowing" > "glow")//true
+console.log("glow" > "glowing")//false
+
+//Comparison With Strings
+console.log("<-----Comparison Strings With Numbers------>")
+console.log("02" > 1)//true
+console.log("100" == 100)//true
+console.log("100" === 100)//false
+
+//Comparison Null and Undefind
+console.log("<-----Comparison Null and Undefind------>")
+
+console.log(null == undefined)//true
+console.log(null === undefined)//false
+console.log(null > 0)//false
+console.log(null < 1)//true
+console.log(null == 0)//false
+console.log(null >= 0)//true
