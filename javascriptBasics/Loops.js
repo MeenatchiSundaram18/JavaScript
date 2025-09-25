a9e0867ba14edd2d2932221d52fb69ad32d53013
meenatchisundaram <meenatchisundaram730@gmail.com>
2025-09-25 16:59:45 +0530

    loops added

--- javascriptBasics/Loops.js
+++ javascriptBasics/Loops.js
@@ -59,3 +59,16 @@ function Xmarkpattern(size){
 
 Xmarkpattern(10)
 
+//While and Do-While
+
+let i = 0
+
+while(i < 10){
+    console.log(i)
+    i = i+1
+}
+
+do {
+    console.log(i);
+    i++
+}while(i<10)
